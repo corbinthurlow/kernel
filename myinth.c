e931ac4ca26c93e1285500fd1f973ce9269cfe03
Shawn Saenger <shawnsaenger@yahoo.com>
2017-11-13 12:53:03 -0700

    added lab6code and commented out pre lab 6 stuff

--- myinth.c
+++ myinth.c
@@ -1,5 +1,48 @@
+
+
 #include "clib.h"
 #include "yakk.h"
+#include "lab6defs.h"
+
+
+extern int KeyBuffer;
+void InterruptTicker();
+void InterruptReseter();
+void InterruptKeyboarder();
+
+extern YKQ *MsgQPtr;
+extern struct msg MsgArray[];
+extern int GlobalFlag;
+
+void InterruptTicker(){
+	
+	static int next = 0;
+	static int data = 0;
+    /* create a message with tick (sequence #) and pseudo-random data */
+    MsgArray[next].tick = YKTickNum;
+    data = (data + 89) % 100;
+    MsgArray[next].data = data;
+    if (YKQPost(MsgQPtr, (void *) &(MsgArray[next])) == 0)
+	printString("  TickISR: queue overflow! \n");
+    else if (++next >= MSGARRAYSIZE)
+	next = 0;
+}
+
+void InterruptReseter(){
+	printNewLine();
+	printString("Reset Pressed");
+	exit(0);
+}
+
+void InterruptKeyboarder(){
+	GlobalFlag = 1;
+}
+
+/* pre lab6 hander code
+
+#include "clib.h"
+#include "yakk.h"
+
 
 int counter = 0;
 extern int KeyBuffer;
@@ -7,22 +50,28 @@ void InterruptTicker();
 void InterruptReseter();
 void InterruptKeyboarder();
 
+
 void InterruptTicker(){
+
+
+
+
 printNewLine();
 counter++; 
 printString("TICK ");
 printInt(counter);
 printNewLine();
+
 }
 
 void InterruptReseter(){
-printNewLine();
-printString("Reset Pressed");
-exit(0);
+	printNewLine();
+	printString("Reset Pressed");
+	exit(0);
 }
 
 void InterruptKeyboarder(){
-	/*int j = 0;
+	int j = 0;
 	if(KeyBuffer == 'd' || KeyBuffer == 'D'){
 		printNewLine();
 		printString("DELAY KEY PRESSED");
@@ -43,6 +92,5 @@ void InterruptKeyboarder(){
 		printString(") IGNORED");
 		printNewLine();
 	}
-*/
-GlobalFlag = 1;
 }
+*/
